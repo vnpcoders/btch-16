ffb8a69cb0c99ba7b280e13b9489ca021f35e0d8
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-08 05:39:39 +0000

    Sep 8, 2025, 11:09 AM

--- NPl/AICalculator/AiCalculator.py
+++ NPl/AICalculator/AiCalculator.py
@@ -3,7 +3,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import LLMMathChain,LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_community.utilities import wikipediaAPIWrapper
-from langchain.agents.agent_types import AgentType
+from langchain.agents.agent_types import TAgentType
 from langchain.callbacks import StdOutCallbackHandler
 
 st.set_page_config(page_title="Text to Math Problem Solver")
