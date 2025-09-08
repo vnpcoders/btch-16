4fe07cb42bf8842a6cd73a5a619203e358b3ec3d
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-08 05:34:33 +0000

    Sep 8, 2025, 11:04 AM

--- NPl/AICalculator/AiCalculator.py
+++ NPl/AICalculator/AiCalculator.py
@@ -37,4 +37,4 @@ Answer:
 """
 question=st.text_input("Enter your problem here")
 prompt_template=PromptTemplate(input_variables=['question'],template=prompt)
-chain=LLMChain
+chain=LLMChain(llm=llm,prompt=pr)
