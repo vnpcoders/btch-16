2b6bcb73e7e1425a5bebbedb0c997d4a5da64665
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-08 05:05:36 +0000

    Sep 8, 2025, 10:35 AM

--- NPl/AICalculator/AiCalculator.py
+++ NPl/AICalculator/AiCalculator.py
@@ -4,4 +4,4 @@ from langchain.chains import LLMMathChain,LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_community.utilities import wikipediaAPIWrapper
 from langchain.agents.agent_types import AgentType
-from langchain.callbacks import StdOutCallback
+from langchain.callbacks import StdOutCallbackH
