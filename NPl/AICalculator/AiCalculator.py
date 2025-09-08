bea84e394f3f93e8ff3721e91ace239db216163d
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-08 05:02:39 +0000

    Sep 8, 2025, 10:32 AM

--- NPl/AICalculator/AiCalculator.py
+++ NPl/AICalculator/AiCalculator.py
@@ -1,4 +1,4 @@
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerative
 from langchain.chains import LLMMathChain,LLMChain
-from langchain.prompts import PromptT
+from langchain.prompts import PromptTempl
