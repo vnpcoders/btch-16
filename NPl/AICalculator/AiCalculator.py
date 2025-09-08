e8fd6bd8ace611770dc73b4ec1c627b09f7c706c
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-08 05:29:54 +0000

    Sep 8, 2025, 10:59 AM

--- NPl/AICalculator/AiCalculator.py
+++ NPl/AICalculator/AiCalculator.py
@@ -31,5 +31,5 @@ calculator=Tool(
 )
 
 prompt="""
-you are my personel agent task with solving users mathamtical questions
+you are my personel agent task with solving users mathamatical questions
 """
