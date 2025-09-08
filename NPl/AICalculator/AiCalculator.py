7cd8c41aaa1c863f08494e9e0f0d092aab9e3162
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-08 06:31:23 +0000

    Sep 8, 2025, 12:01 PM

--- NPl/AICalculator/AiCalculator.py
+++ NPl/AICalculator/AiCalculator.py
@@ -1,84 +1,87 @@
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains import LLMMathChain,LLMChain
+from langchain.chains import LLMMathChain, LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.agents.agent_types import AgentType
-from langchain.agents import Tool,initialize_agent
-from langchain.callbacks import StdOutCallbackHandler
+from langchain.agents import Tool, initialize_agent
+from langchain.callbacks import StreamlitCallbackHandler
 
-st.set_page_config(page_title="Text to Math Problem Solver")
-st.title("Text to Math problem Solver using Gemini")
+st.set_page_config(page_title="Text To Math Problem Solver And Data Search Assistant", page_icon="🧮")
+st.title("Text To Math Problem Solver Using Google Gemini 🚀")
 
-#Gemini ApI Key
-gemini_api_key= st.sidebar.text_input(label="Google Gemini API key",type='password')
-if  not gemini_api_key:
-    st.info("Please add your gemini API Key to Continue")
+#Gemini_API_Key = "AIzaSyBHtrj9Py_7uS8o8tLBYOsh3Qw9I5NPWkA"
+
+gemini_api_key = st.sidebar.text_input(label="Google Gemini API Key", type="password")
+
+if not gemini_api_key:
+    st.info("Please add your Gemini API key to continue")
     st.stop()
 
-llm= ChatGoogleGenerativeAI(model="gemini-2.5-flash",
-                            google_api_key=gemini_api_key)
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=gemini_api_key, temperature=0)
 
-wikipedia_wrapper=WikipediaAPIWrapper()
-wikipedia_Tool=Tool(name="Wikipedia",func=wikipedia_wrapper.run,description="A tool for searching the Internet to find information on various object")
+wikipedia_wrapper = WikipediaAPIWrapper()
+wikipedia_tool = Tool(
+    name="Wikipedia",
+    func=wikipedia_wrapper.run,
+    description="A tool for searching the Internet to find information on various topics."
+)
 
-#math tool
 
-math_chain=LLMMathChain.from_llm(llm=llm)
-calculator=Tool(
+# Math Tool
+math_chain = LLMMathChain.from_llm(llm=llm)
+calculator = Tool(
     name="Calculator",
     func=math_chain.run,
-    description="A tool For answering math related questions"
+    description="A tool for answering math-related questions. Provide only mathematical expressions."
 )
 
-prompt="""
-                  you are my personel agent task with solving users mathamatical questions
-Question=st.text_input("question")
+# Reasoning Tool
+prompt = """
+You are an agent tasked with solving users' mathematical questions. 
+Logically arrive at the solution and provide a detailed explanation.
+Display the steps point-wise.
+
+Question: st.text_input({question})
 Answer:
 """
-question=st.text_input("Enter your problem here")
-prompt_template=PromptTemplate(input_variables=['question'],template=prompt)
-chain=LLMChain(llm=llm,prompt=prompt_template)
+question = st.text_input("Enter your problem here")
+prompt_template = PromptTemplate(input_variables=["question"], template=prompt)
+chain = LLMChain(llm=llm, prompt=prompt_template)
 
-reasoning_tool=Tool(
-    name="Resoning Tool",
-    func= chain.run,
-    description="A tool for answering logic-based and  reasoning question."
-
+reasoning_tool = Tool(
+    name="Reasoning Tool",
+    func=chain.run,
+    description="A tool for answering logic-based and reasoning questions."
 )
 
-assistant_agent=initialize_agent(
-tools=[wikipedia_Tool,calculator,reasoning_tool],
-llm=llm,
-agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-verbose=False,
-handle_parsing_errors=True)
+assistant_agent = initialize_agent(
+    tools=[wikipedia_tool, calculator, reasoning_tool],
+    llm=llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=False,
+    handle_parsing_errors=True
+)
 
 if "messages" not in st.session_state:
-    st.session_state['messages']={
-        'role':'assistant',
-        'content':"Hi,I am a math chatbot powerd by gemini"
-    }
-for msg in st.session_state.messages:
-    st.chat_message(msg['role']).write(msg['content'])
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "Hi, I'm a Math chatbot powered by Gemini. Ask me any math or knowledge-based question!"}
+    ]
 
-if st.button("Find answer"):
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+if st.button("Find my answer"):
     if question:
-        with st.spinner("Generation respones..."):
-            st.session_state.messages.append
-            ({'role':'assistant','content':question})
+        with st.spinner("Generating response..."):
+            st.session_state.messages.append({"role": "user", "content": question})
             st.chat_message("user").write(question)
 
-            st_cb=StdOutCallbackHandler(st.container()
-                                        ,expand_new_thoughts=False)
-            response=assistant_agent.run(st.session_state.messages,collbacks=[st_cb])
-            st.session_state.messages.append
-            ({'role':'assistant','content':response})
+            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            response = assistant_agent.run(st.session_state.messages, callbacks=[st_cb])
 
-            st.write("Response")
+            st.session_state.messages.append({'role': 'assistant', "content": response})
+            st.write('### Response:')
             st.success(response)
     else:
-        st.warning('Please enter your question')
-
-
-
+        st.warning("Please enter a question.")
