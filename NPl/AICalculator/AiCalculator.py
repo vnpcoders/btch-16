e14f90e99bab5b843fe542bd3cbc87f0bfceb99f
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-08 05:22:44 +0000

    Sep 8, 2025, 10:52 AM

--- NPl/AICalculator/AiCalculator.py
+++ NPl/AICalculator/AiCalculator.py
@@ -19,4 +19,4 @@ llm= ChatGoogleGenerativeAI(model="gemini-2.5-flash",
                             google_api_key=gemini_api_key)
 
 wikipedia_wrapper=wikipediaAPIWrapper()
-wikipedia_Tool=Tool(name="Wikipedia",func=wikipedia_wrapper,description="A tool for sear")
+wikipedia_Tool=Tool(name="Wikipedia",func=wikipedia_wrapper,description="A tool for searching")
