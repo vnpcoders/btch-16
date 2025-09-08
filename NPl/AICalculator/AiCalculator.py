011d9016f2c50a793eb469abe10a88ce4095350a
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-08 06:09:32 +0000

    Sep 8, 2025, 11:39 AM

--- NPl/AICalculator/AiCalculator.py
+++ NPl/AICalculator/AiCalculator.py
@@ -71,6 +71,6 @@ if st.button("Find answer"):
 
             st_cb=StdOutCallbackHandler(st.container()
                                         ,expand_new_thoughts=False)
-            response=assistant_agent.run
+            response=assistant_agent.run(st.session_state)
 
 
