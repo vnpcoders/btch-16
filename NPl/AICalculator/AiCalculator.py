a585a6f45f5ad1260414f10aa41190f05937bbce
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-08 05:08:46 +0000

    Sep 8, 2025, 10:38 AM

--- NPl/AICalculator/AiCalculator.py
+++ NPl/AICalculator/AiCalculator.py
@@ -9,4 +9,4 @@ from langchain.callbacks import StdOutCallbackHandler
 st.set_page_config(page_title="Text to Math Problem Solver")
 st.title("Text to Math problem Solver using Gemini")
 
-#G
+#Gemini
