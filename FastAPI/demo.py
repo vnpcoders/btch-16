b29320a24049d702767fb41f406817de3900435f
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-09 06:25:24 +0000

    Sep 9, 2025, 11:55 AM

--- FastAPI/demo.py
+++ FastAPI/demo.py
@@ -17,3 +17,8 @@ def read_out():
 @app.get("/items/{item_id}")
 def read_item(item_id:int,q:str=None):
     return{"item_id":item_id,"query":q}
+
+
+@app.post("/item/{item_id}")
+def create_item(item:Item):
+    return{}
