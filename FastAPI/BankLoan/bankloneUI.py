b9893236d06b40314ceb025a2f868474d3bfcef2
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-09 09:49:56 +0000

    Sep 9, 2025, 3:19 PM

--- FastAPI/BankLoan/bankloneUI.py
+++ FastAPI/BankLoan/bankloneUI.py
@@ -33,7 +33,7 @@ def predict_lone(banklone: banklone):
 
     # Create input DataFrame
     input_df = pd.DataFrame([{
-        "Pclass": passenger.Pclass,
+        "Pclass": passenger.Age,
         "Sex": sex_encoded,
         "Age": passenger.Age,
         "SibSp": passenger.SibSp,
