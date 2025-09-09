e83735d359621625a435537a8f91b6291c56e28d
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-09 08:55:58 +0000

    Sep 9, 2025, 2:25 PM

--- FastAPI/Titanic_fold/Titanic.py
+++ FastAPI/Titanic_fold/Titanic.py
@@ -32,7 +32,7 @@ class Passenger(BaseModel):
 @app.post("/predict")
 def predict_survival(passenger: Passenger):
     # Encode categorical fields
-    sex_encoded = encoders['Sex'].transform([passenger.Sex])[0]
+    sex_encoded = encoders.transform([passenger.Sex])[0]
     embarked_encoded = encoders.transform([passenger.Embarked])[0]
 
     # Create input DataFrame
