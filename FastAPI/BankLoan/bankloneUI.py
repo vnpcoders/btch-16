cc877c82603bf63464af3ffeab98ce1a452fc1e0
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-09 09:25:31 +0000

    Sep 9, 2025, 2:55 PM

--- FastAPI/BankLoan/bankloneUI.py
+++ FastAPI/BankLoan/bankloneUI.py
@@ -14,8 +14,8 @@ class Passenger(BaseModel):
     Experience: Annotated[int, Field(..., gt = 0,description="Experience")]
     Income: Annotated[int, Field(...,gt = 0,description="Income")]
     Family: Annotated[int, Field(...,gt = 1, lt = 20,description="Family Members")]
-    
-    Experience: Annotated[int, Field(..., gt = 0,description="Experience")]
+
+    CCAvg: Annotated[int, Field(..., gt = 0,description="Experience")]
     Income: Annotated[int, Field(...,gt = 0,description="Income")]
     Family: Annotated[int, Field(...,gt = 1, lt = 20,description="Family Members")]
 
