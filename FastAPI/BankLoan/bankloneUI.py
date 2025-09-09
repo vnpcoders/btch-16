26a02c554a7b7de24c53eb63b9c66108ccb3f87c
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-09 09:57:51 +0000

    Sep 9, 2025, 3:27 PM

--- FastAPI/BankLoan/bankloneUI.py
+++ FastAPI/BankLoan/bankloneUI.py
@@ -50,5 +50,5 @@ def predict_lone(banklone: banklone):
 
     # Predict
     prediction = model.predict(input_df)[0]
-    return {"survived": int(prediction)}
+    return {"": int(prediction)}
         
