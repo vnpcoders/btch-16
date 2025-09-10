3eecf82071ceeedf57e08290be70bdfe7885f71f
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-10 05:31:41 +0000

    Sep 10, 2025, 11:01 AM

--- FastAPI/BankLoan/bankloneUI.py
+++ FastAPI/BankLoan/bankloneUI.py
@@ -29,7 +29,7 @@ def predict_lone(banklone: banklone):
     Securities_Account_encoder= 1 if banklone.Securities_Account=="YES"else 0
     CD_Account_encoder=1 if banklone.CD_Account=="YES" else 0
     Online_encoder=1 if banklone.Online=="YES" else 0
-    CreditCard_encoder= 1 if banklone.CreditCard=="YES" else 0
+    CreditCard_encoder= 1 if bankloan.CreditCard=="YES" else 0
 
     # Create input DataFrame
     input_df = pd.DataFrame([{
