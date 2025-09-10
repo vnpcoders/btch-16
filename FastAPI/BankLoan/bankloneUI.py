a89e811fe2b3d1af0a21abb42beab0816bdc9a11
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-10 05:31:55 +0000

    Sep 10, 2025, 11:01 AM

--- FastAPI/BankLoan/bankloneUI.py
+++ FastAPI/BankLoan/bankloneUI.py
@@ -26,7 +26,7 @@ class bankloan(BaseModel):
 def predict_lone(banklone: banklone):
     # Encode categorical fields
     Education_encoded=0 if banklone.Education=="undergraduat"else (1 if banklone.Education=="graduat" else 2 )
-    Securities_Account_encoder= 1 if banklone.Securities_Account=="YES"else 0
+    Securities_Account_encoder= 1 if bankloan.Securities_Account=="YES"else 0
     CD_Account_encoder=1 if bankloan.CD_Account=="YES" else 0
     Online_encoder=1 if bankloan.Online=="YES" else 0
     CreditCard_encoder= 1 if bankloan.CreditCard=="YES" else 0
