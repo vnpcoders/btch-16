8ce7dac15d9ff4de895e35459d62b9df275fe14b
vnpcoders <vasudevpatidar52@gmail.com>
2025-09-10 05:37:15 +0000

    Sep 10, 2025, 11:07 AM

--- FastAPI/BankLoan/bankloneUI.py
+++ FastAPI/BankLoan/bankloneUI.py
@@ -17,7 +17,7 @@ class bankloan(BaseModel):
     CCAvg: Annotated[int, Field(..., gt = 0,description="CCAvg")]
     Education: Annotated[Literal["undergraduat","graduat","postgraduat"], Field(...,description="Education select only 😒undergraduat,😊graduat,😂postgraduat")]
     Mortgage: Annotated[int, Field(...,gt = 0,description="Mortgage")]
-    Securities.Account: Annotated[Literal["YES","NO"], Field(..., description="Securities_Account")]
+    Securities_Account: Annotated[Literal["YES","NO"], Field(..., description="Securities_Account")]
     CD_Account: Annotated[Literal["YES","NO"], Field(..., description="CD_Account")]
     Online: Annotated[Literal["YES","NO"], Field(..., description="Online")]
     CreditCard: Annotated[Literal["YES","NO"], Field(..., description="CreditCard")]
